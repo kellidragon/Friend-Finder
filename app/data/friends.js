6ebc2c81a24f1a72bc25479bd36bc13dc8d86667
kellidragon <dragonkelli@gmail.com>
2019-06-09 16:33:17 -0700

    html + basic func working

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendData = [
+    {
+      customerName: "Suzy",
+      customerPhoto: "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-8/12186598_10153466145559843_4570229049959508188_o.jpg?_nc_cat=100&_nc_oc=AQmWXvnuOc0ch8O9DcXjhlSU2qnGpNUMvq2FelhVd1W3D9ao3EqG_O03O85aFPjD740mJTQTyDDDxt5GiemyS5IF&_nc_ht=scontent-sjc3-1.xx&oh=f71e072a709cb5aa65b3d1dbc346dd43&oe=5D919E50",
+      scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    }
+  ];
+  
+  module.exports = friendData;
+  
